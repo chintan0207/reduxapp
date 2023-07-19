036fcbd4d948746105806e17cbecaa31ca19270a
chintan <imchintan0207@gmail.com>
2023-07-20 01:05:26 +0530

    completed the reduxapp

---
+++ src/components/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Shop from './Shop'
+
+const Home = () => {
+  return (
+    <div className='container my-2'>
+
+    <Shop/>
+    
+    </div>
+  )
+}
+
+export default Home
---
+++ src/components/Shop.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { bindActionCreators } from 'redux';
+import {actionCreators} from '../state/index'
+
+const Shop = () => {
+
+  const dispatch = useDispatch();
+  const balance = useSelector(state => state.amount);
+
+  const {depositMoney,withdrawMoney} = bindActionCreators(actionCreators,dispatch)
+  return (
+    <div>
+
+    <h3>Deposite/Withdraw Money</h3> 
+    {/*<button className="btn btn-primary mx-2" onClick={()=>{dispatch(actionCreators.withdrawMoney(100))}}>-</button>
+      Update Balance
+    <button className="btn btn-primary mx-2" onClick={()=>{dispatch(actionCreators.depositMoney(100))}}>+</button>
+
+  */}
+  <button className="btn btn-primary mx-2" onClick={()=>{withdrawMoney(100)}}>-</button>
+      Update Balance({balance})
+    <button className="btn btn-primary mx-2" onClick={()=>{depositMoney(100)}}>+</button>
+
+    </div>
+  )
+}
+
+export default Shop
